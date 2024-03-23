bdfbbc4051af864ec5c4599d0ed20a6ec766413f
lorenzorodrig64 <lbr5353@psu.edu>
2024-03-23 15:44:45 -0400

    User input in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
 #include "Shape.h"
+#include <iostream>
+
+int main() {
+
+	// declaration of variables
+	double normalStressXX, normalStressYY, normalStressZZ, shearStressXY;
+
+	// instructions to user
+	std::cout << "Insert the values for stress acting on your element.\n+ = tensile stress\n- = compressive stress" << std::endl << std::endl;
+
+	// user input of variables (will later be replaced with sensor input)
+	std::cout << "Enter value for normal stress in the X-direction: ";
+	std::cin >> normalStressXX;
+	std::cout << "Enter value for normal stress in the Y-direction: ";
+	std::cin >> normalStressYY;
+	std::cout << "Enter value for normal stress in the Z-direction: ";
+	std::cin >> normalStressZZ;
+	std::cout << "Enter value for the shear stress in the XY plane: ";
+	std::cin >> shearStressXY;
+	std::cout << std::endl;
+	// end of user input
+
+	return 0;
+}
